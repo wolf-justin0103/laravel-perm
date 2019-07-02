57d9660b9db00d3203ad556d45592e7977d5d42d
wolf-justin0103 <zzz.wolf.justin@gmail.com>
2019-07-02 14:00:47 +0800

    Update Permission.php

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -2,8 +2,8 @@
 
 namespace Spatie\Permission\Test;
 
-use Artisan;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Artisan;
 use Spatie\Permission\Models\Permission;
 
 class CommandTest extends TestCase
@@ -14,6 +14,7 @@ class CommandTest extends TestCase
         Artisan::call('permission:create-role', ['name' => 'new-role']);
 
         $this->assertCount(1, Role::where('name', 'new-role')->get());
+        $this->assertCount(0, Role::where('name', 'new-role')->first()->permissions);
     }
 
     /** @test */
@@ -48,5 +49,76 @@ class CommandTest extends TestCase
         $this->assertCount(1, Permission::where('name', 'new-permission')
             ->where('guard_name', 'api')
             ->get());
+    }
+
+    /** @test */
+    public function it_can_create_a_role_and_permissions_at_same_time()
+    {
+        Artisan::call('permission:create-role', [
+            'name' => 'new-role',
+            'permissions' => 'first permission | second permission',
+        ]);
+
+        $role = Role::where('name', 'new-role')->first();
+
+        $this->assertTrue($role->hasPermissionTo('first permission'));
+        $this->assertTrue($role->hasPermissionTo('second permission'));
+    }
+
+    /** @test */
+    public function it_can_create_a_role_without_duplication()
+    {
+        Artisan::call('permission:create-role', ['name' => 'new-role']);
+        Artisan::call('permission:create-role', ['name' => 'new-role']);
+
+        $this->assertCount(1, Role::where('name', 'new-role')->get());
+        $this->assertCount(0, Role::where('name', 'new-role')->first()->permissions);
+    }
+
+    /** @test */
+    public function it_can_create_a_permission_without_duplication()
+    {
+        Artisan::call('permission:create-permission', ['name' => 'new-permission']);
+        Artisan::call('permission:create-permission', ['name' => 'new-permission']);
+
+        $this->assertCount(1, Permission::where('name', 'new-permission')->get());
+    }
+
+    /** @test */
+    public function it_can_show_permission_tables()
+    {
+        Artisan::call('permission:show');
+
+        $output = Artisan::output();
+
+        $this->assertTrue(strpos($output, 'Guard: web') !== false);
+        $this->assertTrue(strpos($output, 'Guard: admin') !== false);
+
+        // |               | testRole | testRole2 |
+        $this->assertRegExp('/\|\s+\|\s+testRole\s+\|\s+testRole2\s+\|/', $output);
+
+        // | edit-articles |  ·       |  ·        |
+        $this->assertRegExp('/\|\s+edit-articles\s+\|\s+·\s+\|\s+·\s+\|/', $output);
+
+        Role::findByName('testRole')->givePermissionTo('edit-articles');
+        $this->reloadPermissions();
+
+        Artisan::call('permission:show');
+
+        $output = Artisan::output();
+
+        // | edit-articles |  ·       |  ·        |
+        $this->assertRegExp('/\|\s+edit-articles\s+\|\s+✔\s+\|\s+·\s+\|/', $output);
+    }
+
+    /** @test */
+    public function it_can_show_permissions_for_guard()
+    {
+        Artisan::call('permission:show', ['guard' => 'web']);
+
+        $output = Artisan::output();
+
+        $this->assertTrue(strpos($output, 'Guard: web') !== false);
+        $this->assertTrue(strpos($output, 'Guard: admin') === false);
     }
 }
